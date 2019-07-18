58a1314f08b40256e6f48fefd7d904b561987764
Jason Benaim <jkbenaim@gmail.com>
2019-07-18 12:41:55 -0700

    explicitly choose fields in sql insert

--- src/out-sqlite.c
+++ src/out-sqlite.c
@@ -108,7 +108,7 @@ sqlite_out_status(struct Output *out, FILE *fp, time_t timestamp,
 
     sqlite_out_tr_continue(out, fp, 1);
 
-    fprintf(fp, "INSERT INTO sense SELECT val, %ld, \"%s\", %u, %u, %u, \"%s\", null FROM temp.vars WHERE key=='scan_id';\n",
+    fprintf(fp, "INSERT INTO sense(scan_id, time, ip, ip_proto, port, ttl, proto, px) SELECT val, %ld, \"%s\", %u, %u, %u, \"%s\", null FROM temp.vars WHERE key=='scan_id';\n",
             timestamp,
             ip_string,
             ip_proto,
@@ -136,7 +136,7 @@ sqlite_out_banner(struct Output *out, FILE *fp, time_t timestamp,
 
     sqlite_out_tr_continue(out, fp, 1);
     
-    fprintf(fp, "INSERT INTO sense SELECT val, %ld, \"%s\", %u, %u, %u, \"%s\", \"%s\" FROM temp.vars WHERE key=='scan_id';\n",
+    fprintf(fp, "INSERT INTO sense(scan_id, time, ip, ip_proto, port, ttl, proto, px) SELECT val, %ld, \"%s\", %u, %u, %u, \"%s\", \"%s\" FROM temp.vars WHERE key=='scan_id';\n",
             timestamp,
             ip_string,
             ip_proto,
